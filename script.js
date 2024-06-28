61c130faa2cd9bda0cb11feee9cd8a37e665a078
Viktor <bolshakov_83@mail.ru>
2024-06-28 11:56:10 +0300

    Домашнее задание 2

---
+++ script.js
@@ -0,0 +1,59 @@
+// Домашнее задание
+// Задание 1: ""Управление библиотекой книг""
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+// Задание 2: ""Управление списком студентов""
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+// javascript
+
+// Пример использования класса
+// const student1 = new Student(""John Smith"", 16, ""10th grade"");
+// student1.displayInfo();
+// Вывод:
+// Name: John Smith
+// Age: 16
+// Grade: 10th grade
+
+// const student2 = new Student(""Jane Doe"", 17, ""11th grade"");
+// student2.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Age: 17
+// Grade: 11th grade"
+
+
+class Book {
+    constructor(title, author, pages) {
+       this.title = title;
+       this.author = author;
+       this.pages = pages;
+    }
+    displayInfo = () => console.log(`Книга: "${this.title}", автор: "${this.author}", количество страниц: ${this.pages}`);
+ }
+ 
+ const book = new Book("JavaScript для чайников", "К. Минник", 320);
+ book.displayInfo();
+ 
+ class Student {
+    constructor(name, age, grade) {
+       this.name = name;
+       this.age = age;
+       this.grade = grade;
+    }
+    displayInfo = () => console.log(`Студент ${this.name} возраст ${this.age} лет, учится в ${this.grade} классе`);
+ }
+ 
+ const student1 = new Student("John Smith", 16, 10);
+ student1.displayInfo();
+ 
+ const student2 = new Student("Jane Doe", 17, 11);
+ student2.displayInfo();
